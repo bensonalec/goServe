5c9f480330c1350a2eb2f0a4b1c7a42b3c5e4c7a
Alec <alecbenson21@gmail.com>
2020-03-07 21:31:14 -0700

    Added base functionality

---
+++ handlers.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"net/http"
+	"log"
+	"io/ioutil"
+	"fmt"
+	"strings"
+	"html/template"
+
+)
+
+func (s *server) handleIndex() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		//go through the files in the html folder
+		//based on the url being checked for
+		files, err := ioutil.ReadDir("./html/")
+		if err != nil {
+			log.Fatal(err)
+		}
+		var fileList []string
+		var dirList []string
+		for _, f := range files {
+			if(f.IsDir()) {
+				dirList = append(dirList,f.Name())
+			} else {
+				fileList = append(fileList,f.Name())
+			}
+		}
+
+		spl := strings.Split(r.URL.String(),"/")
+
+		if(spl[1] == "") {
+			//serve the home page
+			fmt.Println("Served index")
+			err = servePage(w,r,"index.html")
+
+		} else if(contains(dirList,spl[1]) && len(spl) == 3) {
+			//serve the file in spl[1]
+			//check if spl[1] is a file inside the folder spl[0]
+			innerFiles, err := ioutil.ReadDir("./html/"+spl[1]+"/")
+			if err != nil {
+				log.Fatal(err)
+			}
+	
+			var innerFilesList []string
+			for _,f := range innerFiles {
+				if(!f.IsDir()){
+					innerFilesList = append(innerFilesList,f.Name())
+				}
+				
+			}
+			if(contains(innerFilesList,spl[2]+".html")) {
+				err = servePage(w,r,"/" + spl[1]+ "/" +  spl[2] + ".html")
+				fmt.Println("Serving:","/" + spl[1]+ "/" +  spl[2] + ".html")
+			} else {
+				fmt.Println("Serving 404")
+				err = servePage(w,r,"404.html")
+			}
+			
+		} else if(contains(fileList,spl[1]+".html")) {
+			//serve the file in spl[0]
+			fmt.Println("Serving",spl[1])
+			err = servePage(w,r,""+ spl[1] +".html")
+		} else {
+			//serve the 404 page
+			fmt.Println("Served 404")
+			err = servePage(w,r,"404.html")
+		}
+		if err != nil {
+			fmt.Println("Error loading page")
+			
+		}
+		
+	}
+}
+
+func servePage(w http.ResponseWriter, r *http.Request, pageName string) error{
+	t := template.Must(template.ParseFiles("html/"+pageName))
+	err := t.Execute(w,nil)
+	
+	return err
+
+}
+
+func contains(arr []string, str string) bool {
+	for _, a := range arr {
+	   if a == str {
+		  return true
+	   }
+	}
+	return false
+ }
--- main.go
+++ main.go
@@ -3,61 +3,13 @@ package main
 import (
 	"net/http"
 	"log"
-	"io/ioutil"
-	"fmt"
-	"strings"
+
 )
-
-func (s *server) handleIndex() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		//go through the files in the html folder
-		//based on the url being checked for
-		files, err := ioutil.ReadDir(".")
-		if err != nil {
-			log.Fatal(err)
-		}
-		var fileList []string
-		var dirList []string
-		for _, f := range files {
-			if(f.IsDir()) {
-				dirList = append(dirList,f.Name())
-			} else {
-				fileList = append(fileList,f.Name())
-			}
-		}
-		fmt.Println("Files:",fileList)
-		fmt.Println("Directorys:",dirList)
-		//now parse the url being passed in
-		spl := strings.Split(r.URL.String(),"/")
-		
-		if(spl[0] == "") {
-			//serve the home page
-		} else if(contains(dirList,spl[0]) && len(spl) == 3) {
-			//serve the file in spl[1]
-		} else if(contains(fileList,spl[0])) {
-			//serve the file in spl[0]
-		} else {
-			//serve the 404 page
-		}
-		fmt.Println(spl)
-		
-	}
-}
-
-func contains(arr []string, str string) bool {
-	for _, a := range arr {
-	   if a == str {
-		  return true
-	   }
-	}
-	return false
- }
-  
 
 type server struct {}
 
 func (s *server) serve() {
-	log.Fatalln(http.ListenAndServe(":420", nil))
+	log.Fatalln(http.ListenAndServe(":80", nil))
 }
 
 func (s *server) routes() {
